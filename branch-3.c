0290930276166344117c0c31397d474acf8d399f
QiaoZhiGuo <qiaozhiguo@telincn.com>
2017-06-22 10:19:19 +0800

    create source code branch-3.c

---
+++ branch-3.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+int main(int argc, char *argv[])
+{
+	int i = 0;
+	srand(time(NULL));
+
+	for(i = 0; i < 10; i++)
+	{
+		printf("%d  ",rand()%10 + 1);
+	}
+	printf("\n");
+	return 0;
+}
